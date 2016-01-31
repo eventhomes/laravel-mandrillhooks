7e5de34619839656fbe55cc9ebdb5faf7e12f3c4
Bill <roundedvision@gmail.com>
2016-01-30 16:06:54 -0800

    add config

---
+++ src/config/config.php
@@ -0,0 +1,16 @@
+<?php
+return [
+
+  /*
+  |-------------------------------------------------------------
+  | Webhook Key
+  |-------------------------------------------------------------
+  |
+  | The webhook's authentication key. Use this if
+  | you would like to validate the request signature.
+  |
+  */
+
+  'webhook_key' => null,
+
+];
